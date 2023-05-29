ce73796f2399732dde3279835698e8cda1fd6543
heylukechen <lukechen0114@gmail.com>
2023-05-29 23:26:43 +0200

    third iteraiton: all functions are developed and basic design made

--- app.js
+++ app.js
@@ -38,8 +38,11 @@ app.get("/artist-search", (req, res) => {
   spotifyApi
     .searchArtists(req.query.searchArtistName)
     .then((data) => {
-      console.log("The received data from the API: ", data.body.artists);
-      res.render("artist-search-results", { artists: data.body.artists.items });
+      //console.log("The received data from the API: ", data.body.artists);
+      res.render("artist-search-results", {
+        artists: data.body.artists.items,
+        SearchedTerms: req.query.searchArtistName,
+      });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
@@ -47,23 +50,47 @@ app.get("/artist-search", (req, res) => {
 });
 
 app.get("/albums/:artistId", (req, res, next) => {
-  //   console.log(req.params.artistId);
-  spotifyApi
-    .getArtistAlbums(req.params.artistId)
-    .then((data) => {
-      console.log("The received data from the API: ", data.body.items);
-      res.render("albums", { albums: data.body.items });
+  Promise.all([
+    spotifyApi.getArtist(req.params.artistId),
+    spotifyApi.getArtistAlbums(req.params.artistId),
+  ])
+    .then(([data, artistAlbum]) => {
+      const artistName = data.body.name;
+      const albums = artistAlbum.body.items;
+      res.render("albums", { albums: albums, artistName: artistName });
     })
-    .catch((err) => console.log("The error while search albums: ", err));
+    .catch((err) => console.log(err));
 });
 
 app.get("/tracks-info/:albumId", (req, res, next) => {
-  console.log(req.params);
-  spotifyApi
-    .getAlbumTracks(req.params.albumId)
-    .then((data) => {
-      console.log(data.body);
-      res.render("tracks-info", {tracks: data.body.items});
+  Promise.all([
+    spotifyApi.getAlbum(req.params.albumId),
+    spotifyApi.getAlbumTracks(req.params.albumId),
+  ])
+    .then(([albumData, trackData]) => {
+      const albumName = albumData.body.name;
+      const tracks = trackData.body.items;
+      res.render("tracks-info", {
+        tracks: tracks,
+        albumName: albumName,
+      });
     })
-    .catch((err) => console.log("The error while search albums: ", err));
+    .catch((err) => console.log(err));
+  //console.log(req.params);
+  //   let albumName;
+  //   spotifyApi
+  //     .getAlbum(req.params.albumId)
+  //     .then((data) => {
+  //       albumName = data.body.name;
+  //     })
+  //     .catch((err) => console.log(err));
+
+  //   let tracks;
+  //   spotifyApi
+  //     .getAlbumTracks(req.params.albumId)
+  //     .then((data) => {
+  //       //console.log(data);
+  //       tracks = data.body.items;
+  //     })
+  //     .catch((err) => console.log("The error while search albums: ", err));
 });
